dc779852848ef430a06fa398d58841d3f11c9ee2
keplor <keplor@naver.com>
2020-02-15 15:01:00 +0900

    fix : 당첨번호 테스트 수정

--- src/test/java/com/adaline/Lotto/domain/WinningLottoTest.java
+++ src/test/java/com/adaline/Lotto/domain/WinningLottoTest.java
@@ -47,28 +47,93 @@ public class WinningLottoTest {
     }
 
     @Test
-    public void 당첨번호_1개_일치() {
+    public void 당첨번호_하나_일치() {
         Lotto userLotto = new Lotto(Arrays.asList(
                 new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)
+                new LottoNumber(8),
+                new LottoNumber(9),
+                new LottoNumber(10),
+                new LottoNumber(11),
+                new LottoNumber(12)
         ));
         Assert.assertThat(Rank.MISS, is(winningLotto.match(userLotto)));
     }
 
     @Test
-    public void 당첨번호_2개_일치() {
+    public void 당첨번호_둘_일치() {
         Lotto userLotto = new Lotto(Arrays.asList(
                 new LottoNumber(1),
-                new LottoNumber(2),
-                new LottoNumber(3),
-                new LottoNumber(4),
-                new LottoNumber(5),
-                new LottoNumber(6)
+                new LottoNumber(8),
+                new LottoNumber(9),
+                new LottoNumber(7),
+                new LottoNumber(11),
+                new LottoNumber(2)
         ));
         Assert.assertThat(Rank.MISS, is(winningLotto.match(userLotto)));
+    }
+
+    @Test
+    public void 당첨번호_세개_일치() {
+        Lotto userLotto = new Lotto(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(6),
+                new LottoNumber(9),
+                new LottoNumber(7),
+                new LottoNumber(11),
+                new LottoNumber(2)
+        ));
+        Assert.assertThat(Rank.FIFTH, is(winningLotto.match(userLotto)));
+    }
+
+    @Test
+    public void 당첨번호_네개_일치() {
+        Lotto userLotto = new Lotto(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(6),
+                new LottoNumber(5),
+                new LottoNumber(7),
+                new LottoNumber(11),
+                new LottoNumber(2)
+        ));
+        Assert.assertThat(Rank.FOURTH, is(winningLotto.match(userLotto)));
+    }
+
+    @Test
+    public void 당첨번호_다섯개_일치_보너스볼_불일치() {
+        Lotto userLotto = new Lotto(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(6),
+                new LottoNumber(5),
+                new LottoNumber(8),
+                new LottoNumber(4),
+                new LottoNumber(2)
+        ));
+        Assert.assertThat(Rank.THIRD, is(winningLotto.match(userLotto)));
+    }
+
+    @Test
+    public void 당첨번호_다섯개_일치_보너스볼_일치() {
+        Lotto userLotto = new Lotto(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(6),
+                new LottoNumber(5),
+                new LottoNumber(7),
+                new LottoNumber(4),
+                new LottoNumber(2)
+        ));
+        Assert.assertThat(Rank.SECOND, is(winningLotto.match(userLotto)));
+    }
+
+    @Test
+    public void 당첨번호_여섯개_일치() {
+        Lotto userLotto = new Lotto(Arrays.asList(
+                new LottoNumber(1),
+                new LottoNumber(6),
+                new LottoNumber(5),
+                new LottoNumber(4),
+                new LottoNumber(3),
+                new LottoNumber(2)
+        ));
+        Assert.assertThat(Rank.FIRST, is(winningLotto.match(userLotto)));
     }
 }
