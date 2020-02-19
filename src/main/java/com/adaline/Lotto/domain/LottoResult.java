a2a725b5a174dab27a255f653cd2054304dc60da
keplor <keplor@naver.com>
2020-02-19 18:32:58 +0900

    refactor : LottoResult 클래스, totalPrizeMoney 메소드 리팩토링 및 멤버변수 변경

--- src/main/java/com/adaline/Lotto/domain/LottoResult.java
+++ src/main/java/com/adaline/Lotto/domain/LottoResult.java
@@ -1,31 +1,17 @@
 package com.adaline.Lotto.domain;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LottoResult {
-    private final Map<Rank, Integer> result = new HashMap<>();
-
-    {
-        for (Rank rank : Rank.values()) {
-            result.put(rank, 0);
-        }
-    }
+    private final List<Rank> winningList;
 
     public LottoResult(List<Rank> winningList) {
-        for (Rank rank : winningList) {
-            result.put(rank, result.get(rank) + 1);
-        }
+        this.winningList = winningList;
     }
 
     public long totalPrizeMoney() {
-        return result.keySet()
-                .stream()
-                .mapToLong(rank -> {
-                    int count = result.get(rank);
-                    return rank.getWinningMoney() * count;
-                })
+        return winningList.stream()
+                .mapToLong(Rank::getWinningMoney)
                 .sum();
     }
 }
