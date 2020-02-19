ee14d702801453b49fea9ce69a47514b63bfeb2c
keplor <keplor@naver.com>
2020-02-19 11:52:34 +0900

    mod : 당첨 등수 클래스 수정

--- src/main/java/com/adaline/Lotto/domain/Rank.java
+++ src/main/java/com/adaline/Lotto/domain/Rank.java
@@ -1,19 +1,21 @@
 package com.adaline.Lotto.domain;
 
 public enum Rank {
-    FIRST(6),
-    SECOND(5),
-    THIRD(5),
-    FOURTH(4),
-    FIFTH(3),
-    MISS(0);
+    FIRST(6, 2_000_000_000),
+    SECOND(5, 30_000_000),
+    THIRD(5, 1_500_000),
+    FOURTH(4, 50_000),
+    FIFTH(3, 5_000),
+    MISS(0, 0);
 
     private static final int WIN_MIN = 3;
 
     private int countOfMatch;
+    private int winningMoney;
 
-    Rank(int countOfMatch) {
+    Rank(int countOfMatch, int winningMoney) {
         this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
     }
 
     public static Rank valueOf(int countOfMatch, boolean bonus) {
@@ -39,5 +41,9 @@ public enum Rank {
 
     public int getCountOfMatch() {
         return countOfMatch;
+    }
+
+    public int getWinningMoney() {
+        return winningMoney;
     }
 }
